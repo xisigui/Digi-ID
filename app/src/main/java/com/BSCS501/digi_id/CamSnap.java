68a70e832c40e52724f1c71af7ebdb94a695f33a
John Arcie <johnarcie1914@gmail.com>
2020-12-05 20:25:28 +0800

    activity_cam_snap.xml

---
+++ app/src/main/java/com/BSCS501/digi_id/CamSnap.java
@@ -0,0 +1,14 @@
+package com.BSCS501.digi_id;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CamSnap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam_snap);
+    }
+}
